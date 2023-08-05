49569551b662857a0aa892e04a2574b31f741481
maruf <dev.maruf112@gmail.com>
2023-08-05 23:37:33 +0600

    [Done] books details page furnished

--- src/pages/BooksDetails.tsx
+++ src/pages/BooksDetails.tsx
@@ -2,21 +2,26 @@
 import { Card } from 'flowbite-react';
 export default function BooksDetails() {
     return (
-        <Card
-            horizontal
-            imgSrc="/src/assets/images/book1.png"
-        >
-            <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                <p>
-                    Noteworthy technology acquisitions 2021
+        <>
+            <Card
+                horizontal
+                imgSrc="/src/assets/images/book1.png"
+                className='mx-auto mb-6'
+            >
+                <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                    <p>
+                        Noteworthy technology acquisitions 2021
+                    </p>
+                </h5>
+                <p className="font-normal text-gray-700 dark:text-gray-400">
+                    <p>
+                        Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+                    </p>
                 </p>
-            </h5>
-            <p className="font-normal text-gray-700 dark:text-gray-400">
-                <p>
-                    Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
-                </p>
-            </p>
-        </Card>
+            </Card>
+            {/* <p className='text-center text-3xl underline mb-4'>Reviews</p> */}
+        </>
+
     );
 }
 
