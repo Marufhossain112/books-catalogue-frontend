2461a6d4098956ce9f0fc6e88559fb220a85efa4
maruf <dev.maruf112@gmail.com>
2023-08-07 08:31:15 +0600

    [Done] save state of redux by redux persist

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,14 @@ import './index.css';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './routes';
 import { Provider } from 'react-redux';
-import store from "./redux/store";
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor, store } from './redux/store';
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router}></RouterProvider>
+      <PersistGate loading={null} persistor={persistor}>
+        <RouterProvider router={router}></RouterProvider>
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
 );
--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,17 +1,36 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import { api } from "./features/api/apiSlice";
 import userReducer from "./features/user/userSlice";
+
+// Persist configuration for the regular Redux store
+const reduxPersistConfig = {
+    key: 'root', // Change this key if needed
+    storage,
+};
+const rootReducer = userReducer;
+// Persist configuration for the RTK Query cache
+const rtkQueryPersistConfig = {
+    key: 'rtkQuery', // Change this key if needed
+    storage,
+};
+
+// Create the persisted reducer for the regular Redux store
+const persistedReducer = persistReducer(reduxPersistConfig, rootReducer);
 const store = configureStore({
     reducer: {
-        user: userReducer,
+        persistedReducer,
+        // user: userReducer,
         [api.reducerPath]: api.reducer,
     },
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware().concat(api.middleware),
 });
-
+// Persist the store
+const persistor = persistStore(store);
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
-export default store;
+export { store, persistor };
